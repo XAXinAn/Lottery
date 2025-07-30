bc31ed29d294e122516bbd370176b555756262d8
“XAXinAn” <jianghangximail@gmail.com>
2025-07-30 16:27:46 +0800

    需求限制再一次缩小

--- ruoyi-admin/src/main/java/com/ruoyi/lottery/service/impl/WinTransactionsServiceImpl.java
+++ ruoyi-admin/src/main/java/com/ruoyi/lottery/service/impl/WinTransactionsServiceImpl.java
@@ -121,228 +121,61 @@ public class WinTransactionsServiceImpl implements IWinTransactionsService
         List<Transactions> result = new ArrayList<>();
         List<Transactions> finalResult = new ArrayList<>();
         List<WinTransactions> finalWinResult = new ArrayList<>();
-        if (time == 1) {
-            result.clear();
-            for (Prizes prize : prizesList) {
-                List<BigDecimal> billAmounts = new ArrayList<>();
-                List<Transactions> temp = new ArrayList<>();
-                //取出金额区间内的交易记录
-                List<Transactions> transactionsList = transactionsMapper.selectTransactionsByAmountRange(prize);
-                Transactions maxBillAmountTransaction = null;
-                long looptime = 0;
-                //随机打乱交易记录
-                do {
-                    looptime++;
-                    transactionsList.addAll(temp);
-                    temp.clear();
-                    result.clear();
-                    billAmounts.clear();
-                    List<Transactions> shuffledList = new ArrayList<>(transactionsList);
-                    Collections.shuffle(shuffledList);
-                    int cnt = 0;
-                    Transactions transaction = null;
-                    for (int i = 0; i < transactionsList.size() && cnt < prize.getNum(); i++) {
-                        Collections.shuffle(shuffledList);
-                        transaction = shuffledList.get(0);
-                        //判断本次中奖记录的客户内码和网点号是否存在于当前存储的List
-                        boolean exists = false;
-                        for (Transactions t : temp) {
-                            if (t.getOrgNo().equals(transaction.getOrgNo())) {
-                                exists = true;
-                                break;
-                            }
-                            if (t.getCustIsn().equals(transaction.getCustIsn())) {
-                                exists = true;
-                                break;
-                            }
+        result.clear();
+        for (Prizes prize : prizesList) {
+            List<BigDecimal> billAmounts = new ArrayList<>();
+            List<Transactions> temp = new ArrayList<>();
+            //取出金额区间内的交易记录
+            List<Transactions> transactionsList = transactionsMapper.selectTransactionsByAmountRange(prize);
+            Transactions maxBillAmountTransaction = null;
+            long looptime = 0;
+            //随机打乱交易记录
+            do {
+                looptime++;
+//                transactionsList.addAll(temp);
+                temp.clear();
+                result.clear();
+                billAmounts.clear();
+                List<Transactions> shuffledList = new ArrayList<>(transactionsList);
+                Collections.shuffle(shuffledList);
+                int cnt = 0;
+                Transactions transaction = null;
+                for (int i = 0; i < transactionsList.size() && cnt < prize.getNum(); i++) {
+//                        Collections.shuffle(shuffledList);
+                    transaction = shuffledList.get(i);
+                    //判断本次中奖记录的客户内码和网点号是否存在于当前存储的List
+                    boolean exists = false;
+                    for (Transactions t : temp) {
+                        if (t.getOrgNo().equals(transaction.getOrgNo())) {
+                            exists = true;
+                            break;
                         }
-                        //判断是否符合中奖 客户内码不存在于所有中奖纪录 网点号不存在于本次中奖记录
-                        if (!winTransactionsMapper.existsCust(transaction.getCustIsn()) && !winTransactionsMapper.existsOrgNo(transaction.getOrgNo()) && !exists) {
-                            temp.add(transaction);
-                            billAmounts.add(transaction.getBillAmt());
-                            transactionsList.remove(transaction);
-                            cnt++;
+                        if (t.getCustIsn().equals(transaction.getCustIsn())) {
+                            exists = true;
+                            break;
                         }
                     }
-                    sum = getBillAmtsSum(billAmounts);
-                    if (temp.isEmpty()){
-                        continue;
+                    //判断是否符合中奖 客户内码不存在于所有中奖纪录 网点号不存在于本轮已记录的中奖记录 网点号不存在于本循环抽出的中奖信息中
+                    if (!winTransactionsMapper.existsCust(transaction.getCustIsn()) && !winTransactionsMapper.existsOrgNoWithTime(transaction,time) && !exists) {
+                        temp.add(transaction);
+                        billAmounts.add(transaction.getBillAmt());
+//                            transactionsList.remove(transaction);
+                        cnt++;
                     }
-                    maxBillAmountTransaction = temp.stream().max(Comparator.comparing(Transactions::getBillAmt)).get();
-                } while (sum < prize.getBudget().doubleValue() * (0.95 - looptime * 0.005) || sum >= prize.getBudget().doubleValue() || (prize == prizesList.get(prizesList.size() - 1) && org1MaxWinMapper.getOrg1Status(maxBillAmountTransaction.getOrg1Code()) == 1) );
-                result.addAll(temp);
-                finalResult.addAll(temp);
-                if(prize == prizesList.get(prizesList.size() - 1)){
-                    org1MaxWinMapper.updateOrg1MaxWinStatusTo1(maxBillAmountTransaction.getOrg1Code());
                 }
-                for (Transactions transaction : result) {
-                    winTransactionsMapper.insertWinTransactions(new WinTransactions(time,transaction.getAcctnbr(),transaction.getInpTime(),transaction.getInpDate(),transaction.getXtranno(),transaction.getBillAmt(),transaction.getOrg1Code(),transaction.getOrgNo(),transaction.getCustIsn(),transaction.getCardStatusTxt(),transaction.getCustName(),transaction.getCertNo(),transaction.getCardNo(),transaction.getOrgName()));
+                sum = getBillAmtsSum(billAmounts);
+                if (temp.isEmpty()){
+                    continue;
                 }
+                maxBillAmountTransaction = temp.stream().max(Comparator.comparing(Transactions::getBillAmt)).get();
+            } while (sum < prize.getBudget().doubleValue() * (0.95 - looptime * 0.01) || sum >= prize.getBudget().doubleValue() || (prize == prizesList.get(prizesList.size() - 1) && org1MaxWinMapper.getOrg1Status(maxBillAmountTransaction.getOrg1Code()) == 1) );
+            result.addAll(temp);
+            finalResult.addAll(temp);
+            if(prize == prizesList.get(prizesList.size() - 1)){
+                org1MaxWinMapper.updateOrg1MaxWinStatusTo1(maxBillAmountTransaction.getOrg1Code());
             }
-        }else {
-            result.clear();
-            int totalOrgNoNum = transactionsMapper.getTotalOrgNoNum();
-            int restOrgNoNum = totalOrgNoNum - 50;
-            for (Prizes prize : prizesList) {
-                if(prize == prizesList.get(0)){
-                    List<BigDecimal> billAmounts = new ArrayList<>();
-                    List<Transactions> temp = new ArrayList<>();
-                    List<Transactions> transactionsList = transactionsMapper.selectTransactionsByAmountRangeWithoutLastWin(prize,time-1);
-                    Transactions maxBillAmountTransaction = null;
-                    long looptime = 0;
-                    do {
-                        looptime++;
-                        transactionsList.addAll(temp);
-                        temp.clear();
-                        result.clear();
-                        billAmounts.clear();
-                        List<Transactions> shuffledList = new ArrayList<>(transactionsList);
-                        Collections.shuffle(shuffledList);
-                        int cnt = 0;
-                        Transactions transaction = null;
-                        for (int i = 0; i < transactionsList.size() && cnt < restOrgNoNum; i++) {
-                            Collections.shuffle(shuffledList);
-                            transaction = shuffledList.get(0);
-                            boolean exists = false;
-                            for (Transactions t : temp) {
-                                if (t.getOrgNo().equals(transaction.getOrgNo())) {
-                                    exists = true;
-                                    break;
-                                }
-                                if (t.getCustIsn().equals(transaction.getCustIsn())) {
-                                    exists = true;
-                                    break;
-                                }
-                            }
-                            //判断是否符合中奖 客户内码不存在于所有中奖纪录 网点号不存在于本次中奖记录
-                            if (!winTransactionsMapper.existsCust(transaction.getCustIsn()) && !winTransactionsMapper.existsOrgNoWithTime(transaction, time) && !exists) {
-                                temp.add(transaction);
-                                billAmounts.add(transaction.getBillAmt());
-                                transactionsList.remove(transaction);
-                                cnt++;
-                            }
-                        }
-                        sum = getBillAmtsSum(billAmounts);
-                        if (temp.isEmpty()){
-                            continue;
-                        }
-                        maxBillAmountTransaction = temp.stream().max(Comparator.comparing(Transactions::getBillAmt)).get();
-                    } while (sum < prize.getBudget().doubleValue() * (1.0*restOrgNoNum / prize.getNum().doubleValue()) * (0.95 - looptime * 0.001) || sum >= prize.getBudget().doubleValue() * (1.0*restOrgNoNum / prize.getNum().doubleValue()) * (1 + looptime * 0.001));
-                    result.addAll(temp);
-                    finalResult.addAll(temp);
-
-                    List<Transactions> tempTranscript = temp;
-
-                    double lastsum = sum;
-                    long num = prize.getNum() - temp.size();
-                    transactionsList = transactionsMapper.selectTransactionsByAmountRangeWithLastWin(prize, time - 1);//查询出上一轮中过奖的网点的在本次金额区间内的记录
-                    maxBillAmountTransaction = null;
-                    looptime = 0;
-                    //随机打乱交易记录
-                    do {
-                        looptime++;
-                        transactionsList.addAll(temp);
-                        temp.clear();
-                        result.clear();
-                        billAmounts.clear();
-                        List<Transactions> shuffledList = new ArrayList<>(transactionsList);
-                        Collections.shuffle(shuffledList);
-                        int cnt = 0;
-                        Transactions transaction = null;
-                        for (int i = 0; i < transactionsList.size() && cnt < num; i++) {
-                            Collections.shuffle(shuffledList);
-                            transaction = shuffledList.get(0);
-                            boolean exists = false;
-                            for (Transactions t : temp) {
-                                if (t.getOrgNo().equals(transaction.getOrgNo())) {
-                                    exists = true;
-                                    break;
-                                }
-                                if (t.getCustIsn().equals(transaction.getCustIsn())) {
-                                    exists = true;
-                                    break;
-                                }
-                            }
-                            //判断是否符合中奖 客户内码不存在于所有中奖纪录 网点号不存在于本次中奖记录
-                            if (!winTransactionsMapper.existsCust(transaction.getCustIsn()) && !winTransactionsMapper.existsOrgNoWithTime(transaction, time) && !exists) {
-                                temp.add(transaction);
-                                billAmounts.add(transaction.getBillAmt());
-                                transactionsList.remove(transaction);
-                                cnt++;
-                            }
-                        }
-                        sum = getBillAmtsSum(billAmounts);
-                        List<Transactions> allTransactions = new ArrayList<>(tempTranscript);
-                        allTransactions.addAll(temp);
-                        if (temp.isEmpty()){
-                            continue;
-                        }
-                        maxBillAmountTransaction = allTransactions.stream().max(Comparator.comparing(Transactions::getBillAmt)).get();
-                    } while (lastsum + sum < prize.getBudget().doubleValue() * (0.95 - looptime * 0.001) || lastsum + sum >= prize.getBudget().doubleValue() * (1 + looptime * 0.001) || (prize == prizesList.get(prizesList.size() - 1) && org1MaxWinMapper.getOrg1Status(maxBillAmountTransaction.getOrg1Code()) == 1));
-                    result.addAll(temp);
-                    finalResult.addAll(temp);
-                    if(prize == prizesList.get(prizesList.size() - 1)){
-                        org1MaxWinMapper.updateOrg1MaxWinStatusTo1(maxBillAmountTransaction.getOrg1Code());
-                    }
-                    for (Transactions transaction : result) {
-                        winTransactionsMapper.insertWinTransactions(new WinTransactions(time,transaction.getAcctnbr(),transaction.getInpTime(),transaction.getInpDate(),transaction.getXtranno(),transaction.getBillAmt(),transaction.getOrg1Code(),transaction.getOrgNo(),transaction.getCustIsn(),transaction.getCardStatusTxt(),transaction.getCustName(),transaction.getCertNo(),transaction.getCardNo(),transaction.getOrgName()));
-                    }
-                }else{
-                    List<BigDecimal> billAmounts = new ArrayList<>();
-                    List<Transactions> temp = new ArrayList<>();
-                    //取出金额区间内的交易记录
-                    List<Transactions> transactionsList = transactionsMapper.selectTransactionsByAmountRangeWithLastWin(prize,time-1);
-                    Transactions maxBillAmountTransaction = null;
-                    //随机打乱交易记录
-                    long looptime = 0;
-                    do {
-                        looptime++;
-                        transactionsList.addAll(temp);
-                        temp.clear();
-                        result.clear();
-                        billAmounts.clear();
-                        List<Transactions> shuffledList = new ArrayList<>(transactionsList);
-                        Collections.shuffle(shuffledList);
-                        int cnt = 0;
-                        Transactions transaction = null;
-                        for (int i = 0; i < transactionsList.size() && cnt < prize.getNum(); i++) {
-                            Collections.shuffle(shuffledList);
-                            transaction = shuffledList.get(0);
-                            //判断本次中奖记录的客户内码和网点号是否存在于当前存储的List
-                            boolean exists = false;
-                            for (Transactions t : temp) {
-                                if (t.getOrgNo().equals(transaction.getOrgNo())) {
-                                    exists = true;
-                                    break;
-                                }
-                                if (t.getCustIsn().equals(transaction.getCustIsn())) {
-                                    exists = true;
-                                    break;
-                                }
-                            }
-
-                            if (!winTransactionsMapper.existsCust(transaction.getCustIsn()) && !winTransactionsMapper.existsOrgNoWithTime(transaction, time) && !exists) {
-                                temp.add(transaction);
-                                billAmounts.add(transaction.getBillAmt());
-                                transactionsList.remove(transaction);
-                                cnt++;
-                            }
-                        }
-                        sum = getBillAmtsSum(billAmounts);
-                        if (temp.isEmpty()) {
-                            continue;
-                        }
-                        maxBillAmountTransaction = temp.stream().max(Comparator.comparing(Transactions::getBillAmt)).get();
-                    } while (sum < prize.getBudget().doubleValue() * (0.95 - looptime * 0.01) || sum >= prize.getBudget().doubleValue() * (1 + looptime * 0.01) || (prize == prizesList.get(prizesList.size() - 1) && org1MaxWinMapper.getOrg1Status(maxBillAmountTransaction.getOrg1Code()) == 1) );
-                    result.addAll(temp);
-                    finalResult.addAll(temp);
-                    if(prize == prizesList.get(prizesList.size() - 1)){
-                        org1MaxWinMapper.updateOrg1MaxWinStatusTo1(maxBillAmountTransaction.getOrg1Code());
-                    }
-                    for (Transactions transaction : result) {
-                        winTransactionsMapper.insertWinTransactions(new WinTransactions(time,transaction.getAcctnbr(),transaction.getInpTime(),transaction.getInpDate(),transaction.getXtranno(),transaction.getBillAmt(),transaction.getOrg1Code(),transaction.getOrgNo(),transaction.getCustIsn(),transaction.getCardStatusTxt(),transaction.getCustName(),transaction.getCertNo(),transaction.getCardNo(),transaction.getOrgName()));
-                    }
-                }
+            for (Transactions transaction : result) {
+                winTransactionsMapper.insertWinTransactions(new WinTransactions(time,transaction.getAcctnbr(),transaction.getInpTime(),transaction.getInpDate(),transaction.getXtranno(),transaction.getBillAmt(),transaction.getOrg1Code(),transaction.getOrgNo(),transaction.getCustIsn(),transaction.getCardStatusTxt(),transaction.getCustName(),transaction.getCertNo(),transaction.getCardNo(),transaction.getOrgName()));
             }
         }
         finalResult.sort(Comparator.comparing(Transactions::getBillAmt).reversed());
